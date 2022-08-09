92b573474dd9b29a3b1a7c7dd2fb5ba3038bf8ce
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-08-09 19:19:37 +0700

    feat(text-field): добавляет onChange и передачу ref

--- src/ui/text-field.tsx
+++ src/ui/text-field.tsx
@@ -1,14 +1,32 @@
+import { ChangeEventHandler, ForwardedRef, forwardRef } from 'react';
 import styles from './text-field.module.css';
 
 interface Props {
-  value?: number;
-  onBlur?: () => {}
-  testId?: string
+  value: number;
+  onBlur: () => {}
+  onChange: ChangeEventHandler<HTMLInputElement>
+  testId: string
+  ariaLabel: string
+  ref: ForwardedRef<HTMLInputElement>
 }
 
-export default function TextField({value, onBlur, testId}: Props) {
+function Component({value, onBlur, onChange, testId, ariaLabel, ref}: Partial<Props>) {
 
   return (
-    <input className={styles.TextField} defaultValue={value}  data-testid={testId} onBlur={onBlur} type="number"/>
+    <input
+      ref={ref}
+      className={styles.TextField}
+      value={value}
+      aria-label={ariaLabel}
+      data-testid={testId}
+      onChange={onChange}
+      onBlur={onBlur}
+      type="number"
+    />
   )
 }
+
+
+const TextField = forwardRef((props: Partial<Props>, ref: ForwardedRef<HTMLInputElement>) => Component({...props, ref}))
+
+export default TextField;
