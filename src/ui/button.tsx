5adb2192d93b9485792583c33da3e1688ee9e892
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-08-09 19:22:04 +0700

    feat(button): добавляет свойство aria-label

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -5,17 +5,19 @@ type ButtonProps = {
   variant: 'primary' | 'secondary'
   testId: string
   onClick: ( ) => any;
+  ariaLabel: string
 }
 
 export default function Button({children,
   variant = 'primary',
   testId,
+  ariaLabel,
   onClick
 }: Partial<ButtonProps>) {
   const cssClasses = [styles.Button,styles['Button_variant_'.concat(variant)]].join(" ");
 
   return (
-    <button className={cssClasses} data-testid={testId} onClick={onClick}>
+    <button className={cssClasses} data-testid={testId} onClick={onClick} aria-label={ariaLabel}>
       {children}
     </button>
   )
